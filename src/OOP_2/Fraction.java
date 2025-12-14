fba2cb9e30814242db15248be69473e1dbbb2c2d
Maks Chistyakov <maxon7991@gmail.com>
2025-12-14 12:04:27 +0300

    ООП_2. Инкапсуляция #1. Дробь

---
+++ src/OOP_2/Fraction.java
@@ -0,0 +1,45 @@
+package OOP_2;
+
+public class Fraction {
+    private final int numerator;
+    private final int denominator;
+    private final double result;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        if (denominator < 0) {
+            throw new IllegalArgumentException("Знаменатель должен быть положительным");
+        }
+        this.denominator = denominator;
+        this.result = (double) numerator / denominator;
+    }
+
+    public Fraction sum(Fraction inputFraction) {
+        int resultNumerator = this.numerator * inputFraction.denominator + inputFraction.numerator * this.denominator;
+        int resultDenominator = this.denominator * inputFraction.denominator;
+        return new Fraction(resultNumerator, resultDenominator);
+    }
+
+    public Fraction sum(int number) {
+        int resultNumerator = this.numerator * 1 + number * this.denominator;
+        int resultDenominator = this.denominator;
+        return new Fraction(resultNumerator, resultDenominator);
+    }
+
+    public Fraction minus(Fraction inputFraction) {
+        int resultNumerator = this.numerator * inputFraction.denominator - inputFraction.numerator * this.denominator;
+        int resultDenominator = this.denominator * inputFraction.denominator;
+        return new Fraction(resultNumerator, resultDenominator);
+    }
+
+    public Fraction minus(int number) {
+        int resultNumerator = this.numerator * 1 - number * this.denominator;
+        int resultDenominator = this.denominator;
+        return new Fraction(resultNumerator, resultDenominator);
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator + " = " + result;
+    }
+}
