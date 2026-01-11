93e2db460083b106608aa2d76e885c1c6fd852fc
Maks Chistyakov <maxon7991@gmail.com>
2026-01-11 12:52:01 +0300

    ООП_4. Исключения #2. Необрабатываемые исключения

--- src/OOP_4/Sum.java
+++ src/OOP_4/Sum.java
@@ -3,14 +3,14 @@ package OOP_4;
 public class Sum {
     public static void main(String[] args) {
         double sum = 0;
+
+        String checkDouble = "^-?(?:\\d+\\.?\\d*|\\.\\d+)([eE][-+]?\\d+)?$";
+
         for (int i = 0; i < args.length; i++) {
-            try {
+            if (args[i].matches(checkDouble)) {
                 sum += Double.parseDouble(args[i]);
-            } catch (NumberFormatException ex) {
-                args[i] = "0";
             }
         }
-
         System.out.println(sum);
     }
 }
